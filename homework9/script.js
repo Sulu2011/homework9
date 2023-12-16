753e660f088654fa5e156d0fa9a119dd1a77085d
Sulu2011 <126379528+Sulu2011@users.noreply.github.com>
2023-12-16 13:55:49 +0400

    Add files via upload

---
+++ homework9/script.js
@@ -0,0 +1,63 @@
+
+//let s = new Set([]);
+//let x = prompt("Sevimli rənglərinizi daxil edin.");
+//let a=prompt("ne etmek istiyirsiz?(sile ve elave ede bilersiz)")
+//s.add(x);
+//if(a=="silmek"){
+ //   let silmek=prompt("neyi silmek isteyirsiz?");
+ //  s.delete(silmek);
+//}
+//else if(a=="elave etmek"){
+  //  let elave=prompt("ne elave etmek isteyirsiz?");
+   // s.add(elave);
+//}
+
+//console.log(s);//birinci tapsiriq bitdi
+//let gun=new Set(['bazarertesi','cersenbeaxsami','cersenbe',"cumeaxsam;","cume",'senbe','bazar']);
+//let g=prompt("gun elave edek yoxsa silek?")
+//if(g=="silmek"){
+//      let gunsilinir=prompt("neyi silmek isteyirsiz?");
+  //   gun.delete(gunsilinir);
+//}
+//else if(g=="elave etmek"){
+ // let guneelaveolunur=prompt("ne elave etmek isteyirsiz?");
+  // gun.add(guneelaveolunur);
+//}
+//console.log(gun)//muellim burda 1,2ci tapsiriqlar demek olar eyni idi.
+//2ci tapsiriq bitmisdir
+//3 də kesisenlerin nece yazildigini unutdugumu anladim siz cavab verene qeder 4u yazmaga basladim
+//let x=prompt('eded daxil edin ');
+//let a=parseInt(x);
+///let y=prompt("ikinci eded daxil edin");
+//let b=parseInt(y);
+//let result= Math.floor(Math.random(a) * (b));
+//console.log(result);
+//4cu tapsiriq bitmisdir. 5e kecirem 3 uncunun nece yazilacagini helede bilmirem.
+///let x=prompt("eded daxil edin");
+//let c=parseInt(x);
+
+//let y=prompt("eded daxil edin");
+//let d=parseInt(y);
+
+
+
+
+ //function   calculator(a,b){
+
+ ///   result=a+b
+  //  cavab=a*b
+ //   return cavab&&result;
+ //}
+       
+    
+    
+
+
+//console.log(calculator(c,d))//bu tapsiriqda callbackin harda istifade etmeli oldugumu basa dusmedim.
+//tapsiriq 5 bitmisdir.
+let eded=prompt('kubunu tapmaq istediyiniz ededi daxil edin')
+let number=parseInt(eded);
+let kub=Math.pow(number,3);
+
+console.log(kub);//bu tapsiriq 6ci idi men callback movzusunu tam basa dusmedim zehmet olmasa dersde izad edrsiz
+//6ci tapsiriq bitmisdir.
